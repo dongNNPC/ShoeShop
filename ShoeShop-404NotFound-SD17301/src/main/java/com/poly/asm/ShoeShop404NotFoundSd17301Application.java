fcb2f7ad4866eaaf7df44071150defa81f12ee4e
KhanhTran <trantankhanh31102003@gmail.com>
2023-05-21 21:38:26 +0700

    Tran Khanh cập nhật lại giao diện UI

---
+++ ShoeShop-404NotFound-SD17301/src/main/java/com/poly/asm/ShoeShop404NotFoundSd17301Application.java
@@ -0,0 +1,13 @@
+package com.poly.asm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoeShop404NotFoundSd17301Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoeShop404NotFoundSd17301Application.class, args);
+	}
+
+}
