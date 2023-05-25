e71d34f72fab3c7acbfde75f6abe4651a7067111
NguyenDong <0393618987dong@gmail.com>
2023-05-25 22:21:15 +0700

    nhutDong - đã thêm 3 Service : CookieService ,ParamService,SessionService

---
+++ ShoeShop-404NotFound-SD17301/src/main/java/com/poly/asm/service/SessionService.java
@@ -0,0 +1,30 @@
+package com.poly.asm.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import jakarta.servlet.http.HttpSession;
+
+@Service
+public class SessionService {
+    @Autowired
+    HttpSession session;
+    
+   
+    public <T> T get(String name) {
+        // Đọc giá trị của attribute từ session và ép kiểu về kiểu dữ liệu T
+        return (T) session.getAttribute(name);
+    }
+    
+    
+    public void set(String name, Object value) {
+        // Thiết lập giá trị mới cho attribute trong session
+        session.setAttribute(name, value);
+    }
+    
+   
+    public void remove(String name) {
+        // Xóa attribute khỏi session
+        session.removeAttribute(name);
+    }
+}
