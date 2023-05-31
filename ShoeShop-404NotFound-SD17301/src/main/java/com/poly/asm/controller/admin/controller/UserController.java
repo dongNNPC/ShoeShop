c588f9ccc2226200bdd75f52e81a5915d543e045
KhanhTran <trantankhanh31102003@gmail.com>
2023-06-01 01:15:02 +0700

    Khanh đã làm xong user

--- ShoeShop-404NotFound-SD17301/src/main/java/com/poly/asm/controller/admin/controller/UserController.java
+++ ShoeShop-404NotFound-SD17301/src/main/java/com/poly/asm/controller/admin/controller/UserController.java
@@ -5,11 +5,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.poly.asm.dao.UserRepository;
 import com.poly.asm.model.User;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/shoeshop/admin/list-user")
@@ -29,23 +33,37 @@ public class UserController {
 
 	@RequestMapping("/edit/{id}")
 	public String edit(Model model, @PathVariable("id") String id) {
-
+		String successMessage = "Update successful";
+		model.addAttribute("successMessage", successMessage);
 		User user = dao.findById(id).get();
-
 		model.addAttribute("user", user);
 		List<User> users = dao.findAll();
 		model.addAttribute("users", users);
-		return "/admin/views/list-user";
+		return "/admin/views/ui-user";
+
 	}
 
 	@RequestMapping("/create")
-	public String create(User user) {
+	public String create(Model model, @Valid @ModelAttribute("user") User user, BindingResult rs) {
+		if (rs.hasErrors()) {
+			String successMessage = "create failed";
+			model.addAttribute("failed", successMessage);
+			return "/admin/views/ui-user";
+		}
 		dao.save(user);
-		return "redirect:/shoeshop/admin/list-user";
+		String successMessage = "Create successful";
+		model.addAttribute("successMessage", successMessage);
+		return "/admin/views/ui-user";
 	}
 
 	@RequestMapping("/update")
-	public String update(User user) {
+	public String update(Model model, @Valid @ModelAttribute("user") User user, BindingResult rs) {
+		if (rs.hasErrors()) {
+			String successMessage = "Update failed";
+			model.addAttribute("Updatefailed", successMessage);
+			return "/admin/views/ui-user";
+		}
+
 		dao.save(user);
 		return "redirect:/shoeshop/admin/list-user/edit/" + user.getID();
 	}
