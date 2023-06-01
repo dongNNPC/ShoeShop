3f65d9828b73ae04baf5da0285bd5465ebdc7167
NguyenDong <0393618987dong@gmail.com>
2023-06-01 22:24:52 +0700

    nhutDong - đổ db lên index

--- ShoeShop-404NotFound-SD17301/src/main/java/com/poly/asm/model/Product.java
+++ ShoeShop-404NotFound-SD17301/src/main/java/com/poly/asm/model/Product.java
@@ -5,11 +5,15 @@ import java.util.List;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.NamedQuery;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+
+
 
 @Data
 @AllArgsConstructor
@@ -39,6 +43,7 @@ public class Product {
 
 	@OneToMany(mappedBy = "product")
 	List<DetailedImage> detailedImages;
+
 
 	// Các trường khác và getter/setter
 }
