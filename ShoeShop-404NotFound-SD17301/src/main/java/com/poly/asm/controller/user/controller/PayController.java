f5d1f4c379dec1d1374ec01571159fa730b7c998
MinhKhoi01 <nguyenminhkhoi532@gmail.com>
2023-06-09 10:23:34 +0700

    minh khôi hoàn thành trang cá nhân và thanh toán lên dữ liệu

---
+++ ShoeShop-404NotFound-SD17301/src/main/java/com/poly/asm/controller/user/controller/PayController.java
@@ -0,0 +1,45 @@
+package com.poly.asm.controller.user.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.poly.asm.dao.ProductRepository;
+import com.poly.asm.dao.UserRepository;
+import com.poly.asm.model.User;
+import com.poly.asm.service.SessionService;
+
+
+@Controller
+@RequestMapping("/shoeshop")
+public class PayController {
+	@Autowired
+	SessionService session;
+	@Autowired
+	UserRepository dao; //user
+	@Autowired
+	ProductRepository Pdao; //sản phẩm
+	
+	
+
+	
+	
+	@RequestMapping("/thanhtoan")
+	public String thanhtoan(@ModelAttribute("user") User user, Model model) {
+		if (session.get("user") == null) {
+			// Xử lý khi session là null
+			// Ví dụ: Tạo một đối tượng User mặc định
+			User defaultUser = new User();
+			model.addAttribute("user", defaultUser);
+		} else {
+			user = session.get("user");
+			// System.out.println(user.getImage() + "ssssssssssssssssssssssssssss");
+			model.addAttribute("user", user);
+		}
+		return "/views/thanhtoan";
+	}
+
+}
