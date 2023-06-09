2f7929baaaa99f0694c7cd362618dec66c220da9
MinhKhoi01 <nguyenminhkhoi532@gmail.com>
2023-06-09 18:11:09 +0700

    minh khôi đã hoàn thành danh mục

--- ShoeShop-404NotFound-SD17301/src/main/java/com/poly/asm/controller/user/controller/PayController.java
+++ ShoeShop-404NotFound-SD17301/src/main/java/com/poly/asm/controller/user/controller/PayController.java
@@ -9,7 +9,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.poly.asm.controller.IndexController;
@@ -19,6 +22,8 @@ import com.poly.asm.model.Product;
 import com.poly.asm.model.User;
 import com.poly.asm.service.SessionService;
 import com.poly.asm.service.ShoppingCartService;
+
+import jakarta.websocket.server.PathParam;
 
 @Controller
 @RequestMapping("/shoeshop")
@@ -37,7 +42,7 @@ public class PayController {
 	private ShoppingCartService cart;//gio hang
 	
 
-	@RequestMapping("/thanhtoan")
+	@GetMapping("/thanhtoan")
 	public String thanhtoan(@ModelAttribute("user") User user, Model model) {
 		
 		indexController.checkUser(model);
@@ -49,17 +54,28 @@ public class PayController {
 		for (Product p1 : products) {
 			for (Product p2 : products2) {
 				if (p1.getId().equalsIgnoreCase(p2.getId())) {
+					p2.setQuantity(1);
 					products3.add(p2);
 					 totalAmount += p2.getPrice();
+					 
 				}
 			}
 		}
 		
 		model.addAttribute("cart", products3);
+		
 		//tạo biến Tổng ti lưu  tạm trong modal
 		model.addAttribute("totalAmount", totalAmount);
 		model.addAttribute("name", products);
 		return "/views/thanhtoan";
+	}
+	
+	@PostMapping("/create")
+	public String createPay(Model model, @ModelAttribute("product") Product product) {
+
+		
+		return "/views/thanhtoan";
+		
 	}
 
 }
