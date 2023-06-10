d000634e682def8a647eed9b4045ee45715794cc
NguyenDong <0393618987dong@gmail.com>
2023-06-10 16:48:49 +0700

    nhutDong tạo đường dẫn cho các trang

---
+++ ShoeShop-404NotFound-SD17301/src/main/java/com/poly/asm/interceptor/InterConfig.java
@@ -0,0 +1,23 @@
+package com.poly.asm.interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.poly.asm.service.GlobalInterceptor;
+
+@Configuration
+public class InterConfig implements WebMvcConfigurer {
+	
+	@Autowired
+	GlobalInterceptor global;
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+	registry.addInterceptor(global)
+	.addPathPatterns("/**")
+	.excludePathPatterns("/assets/**");
+
+	}
+
+}
