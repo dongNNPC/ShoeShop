632d6d5aa87c4f402d3c9c49b86082ab1bcb8c4a
KhanhTran <trantankhanh31102003@gmail.com>
2023-06-14 09:53:40 +0700

    tranKhanh Bổ sunng đồ thị doanh thu, lọc theo năm, Tạo thông báo cho admin khi có người dùng nào mua hàng và cần xác nhận,Gửi mail cho tất cả user khi có thông báo mới

--- ShoeShop-404NotFound-SD17301/src/main/java/com/poly/asm/controller/IndexAdminController.java
+++ ShoeShop-404NotFound-SD17301/src/main/java/com/poly/asm/controller/IndexAdminController.java
@@ -74,13 +74,19 @@ public class IndexAdminController {
 	@Autowired
 	private IndexController indexController;
 
+	public void notification(Model model) {
+		long quantityPending = invoiceRepository.countByStatus("pending");
+		model.addAttribute("quantityPending", quantityPending);
+	}
+
 	@RequestMapping("/index")
-	public String indexAdmin(Model model, @ModelAttribute("user") User user,
-			@RequestParam(defaultValue = "0") int page) {
+	public String indexAdmin(Model model, @ModelAttribute("user") User user, @RequestParam(defaultValue = "0") int page,
+			@RequestParam(name = "year", required = false) Integer selectedYear) {
 		model.addAttribute("index", "active");
 
 		indexController.checkUser(model);
 
+		notification(model);
 		// tổng số lượng sản phẩm
 		List<Report> reports = daoProduct.getTotalQuantity();
 		model.addAttribute("rpIndex", reports);
@@ -99,7 +105,17 @@ public class IndexAdminController {
 
 //		Biểu đồ sales
 //		List<Integer> salesData = Arrays.asList(2115, 1562, 1584, 1892, 1587, 1923, 2566, 2448, 2805, 3438, 2917, 3327);
-		List<MonthlySalesStatistics> ListSave = invoiceRepository.getMonthlySalesStatistics();
+		List<Integer> yearIntegers = invoiceRepository.getAllYears();
+		model.addAttribute("years", yearIntegers);
+		List<MonthlySalesStatistics> ListSave = new ArrayList<>();
+		if (selectedYear != null) {
+			ListSave = invoiceRepository.getMonthlySalesStatistics(selectedYear);
+
+		} else {
+			ListSave = invoiceRepository.getMonthlySalesStatistics(2023);
+		}
+		model.addAttribute("yearUpdate", selectedYear);
+
 		List<Integer> salesData = new ArrayList<>();
 
 		for (MonthlySalesStatistics monthlySalesStatistics : ListSave) {
@@ -159,6 +175,7 @@ public class IndexAdminController {
 			@RequestParam("keywords") Optional<String> kw, @RequestParam("p") Optional<Integer> p,
 			@RequestParam("field") Optional<String> field, HttpServletRequest request) {
 		model.addAttribute("pending", "active");
+		notification(model);
 
 		indexController.checkUser(model);
 		String order = request.getParameter("sortOrder");
@@ -183,6 +200,7 @@ public class IndexAdminController {
 			Page<Invoice> items = invoiceRepository.findByStatus("pending", pageable);
 			model.addAttribute("invoices", items);
 		}
+
 		return "/admin/views/manager-invoice-pending";
 	}
 
@@ -191,7 +209,7 @@ public class IndexAdminController {
 			@RequestParam("keywords") Optional<String> kw, @RequestParam("p") Optional<Integer> p,
 			@RequestParam("field") Optional<String> field, HttpServletRequest request) {
 		model.addAttribute("pending", "active");
-
+		notification(model);
 		indexController.checkUser(model);
 		String order = request.getParameter("sortOrder");
 
@@ -225,6 +243,7 @@ public class IndexAdminController {
 			@RequestParam("keywords") Optional<String> kw, @RequestParam("p") Optional<Integer> p,
 			@RequestParam("field") Optional<String> field, HttpServletRequest request) {
 		model.addAttribute("delivered", "active");
+		notification(model);
 		indexController.checkUser(model);
 		String order = request.getParameter("sortOrder");
 
@@ -258,6 +277,7 @@ public class IndexAdminController {
 	public String invoiceCancelled(Model model, @ModelAttribute("user") User user,
 			@RequestParam("keywords") Optional<String> kw, @RequestParam("p") Optional<Integer> p,
 			@RequestParam("field") Optional<String> field, HttpServletRequest request) {
+		notification(model);
 		model.addAttribute("cancelled", "active");
 
 		indexController.checkUser(model);
@@ -298,7 +318,7 @@ public class IndexAdminController {
 	@RequestMapping("/ui-user")
 	public String listUIUsetAdmin(@ModelAttribute("user") User user, Model model) {
 		model.addAttribute("ui_user", "active");
-
+		notification(model);
 		indexController.checkUser(model);
 		return "/admin/views/ui-user";
 	}
@@ -313,7 +333,7 @@ public class IndexAdminController {
 	@RequestMapping("/ui-brand")
 	public String listUIBrandAdmin(Model model, @ModelAttribute("user") User user) {
 		model.addAttribute("ui_brand", "active");
-
+		notification(model);
 		indexController.checkUser(model);
 		Brand item = new Brand();
 		model.addAttribute("brand", item);
@@ -324,6 +344,7 @@ public class IndexAdminController {
 	public String listUICategoryAdmin(Model model, @ModelAttribute("user") User user) {
 		model.addAttribute("ui_category", "active");
 		indexController.checkUser(model);
+		notification(model);
 		Category item = new Category();
 		model.addAttribute("category", item);
 
@@ -336,7 +357,7 @@ public class IndexAdminController {
 		model.addAttribute("ui_product", "active");
 
 		indexController.checkUser(model);
-
+		notification(model);
 		DetailedImage detailedImage = new DetailedImage();
 		model.addAttribute("detailedImage", detailedImage);
 //		Danh mục
@@ -358,7 +379,7 @@ public class IndexAdminController {
 			@RequestParam("keywords") Optional<String> kw, @RequestParam("p") Optional<Integer> p,
 			@RequestParam("field") Optional<String> field, HttpServletRequest request) {
 		model.addAttribute("list_brand", "active");
-
+		notification(model);
 		indexController.checkUser(model);
 
 		String order = request.getParameter("sortOrder");
@@ -401,7 +422,7 @@ public class IndexAdminController {
 		model.addAttribute("list_category", "active");
 
 		indexController.checkUser(model);
-
+		notification(model);
 		String order = request.getParameter("sortOrder");
 
 		Sort sort = Sort.by(Direction.ASC, field.orElse("name"));
@@ -438,7 +459,7 @@ public class IndexAdminController {
 			@RequestParam("keywords") Optional<String> kw, @RequestParam("p") Optional<Integer> p,
 			@RequestParam("field") Optional<String> field, HttpServletRequest request) {
 		model.addAttribute("list_product", "active");
-
+		notification(model);
 		indexController.checkUser(model);
 		String order = request.getParameter("sortOrder");
 
@@ -471,7 +492,7 @@ public class IndexAdminController {
 		model.addAttribute("list_user", "active");
 
 		indexController.checkUser(model);
-
+		notification(model);
 		String order = request.getParameter("sortOrder");
 
 		Sort sort = Sort.by(Direction.ASC, field.orElse("name"));
@@ -507,7 +528,7 @@ public class IndexAdminController {
 	public String listStockAdmin(Model model, @ModelAttribute("user") User user,
 			@RequestParam("keywords") Optional<String> kw, @RequestParam("p") Optional<Integer> p,
 			@RequestParam("field") Optional<String> field, HttpServletRequest request) {
-
+		notification(model);
 		model.addAttribute("list_stock", "active");
 		indexController.checkUser(model);
 		String order = request.getParameter("sortOrder");
@@ -518,7 +539,6 @@ public class IndexAdminController {
 				sort = Sort.by(Direction.DESC, field.orElse("price"));
 			}
 		}
-
 		session.remove("keywords");
 		String kwords = kw.orElse(session.get("keywords"));
 		session.set("keywords", kwords, 30);
--- ShoeShop-404NotFound-SD17301/src/main/java/com/poly/asm/dao/InvoiceRepository.java
+++ ShoeShop-404NotFound-SD17301/src/main/java/com/poly/asm/dao/InvoiceRepository.java
@@ -16,28 +16,38 @@ import com.poly.asm.model.MonthlySalesStatistics;
 public interface InvoiceRepository extends JpaRepository<Invoice, String> {
 
 //	câu truy vấn in ra số lượng sản phẩm bán đi theo tháng
-	@Query("SELECT new com.poly.asm.model.MonthlySalesStatistics (MONTH(orderDate), COUNT(*)) FROM Invoice WHERE status = 'delivered' GROUP BY MONTH(orderDate)")
-	List<MonthlySalesStatistics> getMonthlySalesStatistics();
+//	@Query("SELECT new com.poly.asm.model.MonthlySalesStatistics (MONTH(orderDate), COUNT(*)) FROM Invoice WHERE status = 'delivered' GROUP BY MONTH(orderDate)")
+//	List<MonthlySalesStatistics> getMonthlySalesStatistics();
+//	lấy ra tất cả năm (admin index)
+	@Query("SELECT DISTINCT YEAR(i.orderDate) FROM Invoice i")
+	List<Integer> getAllYears();
 
-//	Câu truy vấn in ra  hóa đơn cần duyệt có page
+//	câu truy vấn in ra số lượng sản phẩm bán đi theo tháng và chọn năm (admin index)
+	@Query("SELECT new com.poly.asm.model.MonthlySalesStatistics(MONTH(i.orderDate), COUNT(*)) FROM Invoice i WHERE i.status = 'delivered' AND YEAR(i.orderDate) = :year GROUP BY MONTH(i.orderDate)")
+	List<MonthlySalesStatistics> getMonthlySalesStatistics(@Param("year") int year);
+
+//	Câu truy vấn in ra  hóa đơn cần duyệt (admin index)
 	Page<Invoice> findByStatus(String status, Pageable pageable);
 
-//	danh sách các hóa đơn có trạng thái status hoặc có id chứa keywords bên user.
+//	Lọc số lượng hóa đơn theo trạng thái
+	Long countByStatus(String status);
+
+//	danh sách các hóa đơn có trạng thái status hoặc có id chứa keywords bên user.(user) 
 	@Query("SELECT i FROM Invoice i WHERE i.user.id = :userId AND i.id LIKE %:keywords% AND i.status IN :statusList")
 	Page<Invoice> findByUserIdAndIdContainingAndStatus(@Param("userId") String userId,
 			@Param("keywords") String keywords, @Param("statusList") List<String> statusList, Pageable pageable);
 
-//Truy vấn hóa đơn theo user và status bên user
+//Truy vấn hóa đơn theo user và status bên user (user)
 	@Query("SELECT i FROM Invoice i WHERE i.user.id = :userId AND (i.status = :status1 OR i.status = :status2)")
 	Page<Invoice> findByUserIdAndStatus(@Param("userId") String userId, @Param("status1") String status1,
 			@Param("status2") String status2, Pageable pageable);
 
-//	danh sách các hóa đơn có trạng thái status hoặc có id chứa keywords.
+//	danh sách các hóa đơn có trạng thái status hoặc có id chứa keywords. (admin index)
 	@Query("SELECT i FROM Invoice i WHERE i.status = :status AND i.id LIKE %:kwords%")
 	Page<Invoice> findByStatusAndIdContaining(@Param("status") String status, @Param("kwords") String kwords,
 			Pageable pageable);
 
-//	Truy vấn hóa đơn theo user
+//	Truy vấn hóa đơn theo use
 	@Query("SELECT i FROM Invoice i WHERE i.user.iD = :userId")
 	Page<Invoice> findByUserId(@Param("userId") String userId, Pageable pageable);
 
